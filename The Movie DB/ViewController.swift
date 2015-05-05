da7f3963dce91d8a41365e29635569d19642a061
Jonathan Pacheco <jonathan261090@gmail.com>
2015-05-05 09:07:37 -0500

    Initial Commit

---
+++ The Movie DB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  The Movie DB
+//
+//  Created by Jonathan Pacheco on 5/05/15.
+//  Copyright (c) 2015 ArandaSoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
